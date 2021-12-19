3829b97f4166c5ba050cf68c714ddb156646ec94
AjdinH03 <ajdinhusetovicc03@gmail.com>
2021-12-19 11:05:21 +0100

    commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const title = document.querySelector('#title');
+const playPrompt = document.querySelector('#play-prompt');
+const newGame = document.querySelector('#new-game');
+const reset = document.querySelector('#reset');
